d071793ebb46f7645d76725a55f184cb1e2d6cf8
joinssjup <ducthuykh23@gmail.com>
2026-08-24 02:22:18 +0200

    Add strength.js for sable-password-strength-knit

---
+++ strength.js
@@ -0,0 +1,6 @@
+const PROJECT = "sable-password-strength-knit";
+const PROFILE = "0026";
+function run(value = 'ready') {
+  return { project: PROJECT, profile: PROFILE, value };
+}
+console.log(run());
